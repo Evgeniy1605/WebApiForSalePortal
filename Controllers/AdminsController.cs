6f2da74284d05483ed3fb166cc544ddaf8568416
pikh1 <pikh1@LAPTOP-2LAUS2MF>
2022-12-15 17:49:16 +0200

    Added AdminsController

---
+++ Controllers/AdminsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SalePortal.Data;
+using SalePortal.Entities;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApiForSalePortal.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminsController : ControllerBase
+    {
+        private readonly SalePortalDbConnection _context;
+        public AdminsController(SalePortalDbConnection context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AdminEntity>>> GetAdmins()
+        {
+            return await _context.admins.ToListAsync();
+        }
+
+        
+    }
+}
