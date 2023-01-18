266d64eef1c82ee46c82cf0c5c95252e17a62898
pikh1 <pikh1@LAPTOP-2LAUS2MF>
2023-01-18 13:43:02 +0200

    Finished the main logic

--- Controllers/NovaPoshtaController.cs
+++ Controllers/NovaPoshtaController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using WebApiForSalePortal.Services;
 
@@ -14,6 +15,7 @@ namespace WebApiForSalePortal.Controllers
             _novaPoshtaService = novaPoshtaService;
         }
         [HttpGet]
+        [Authorize]
         public async ValueTask<IActionResult> GetAdressesByCityName(string cityName)
         {
 
